806fae290dc7b955f8b138bca8eaa6fb4a6a5c10
EIlif Johansen <eilifjohansen@outlook.com>
2023-12-24 13:58:41 +0100

    Redesignet footer

--- src/Accessibility.tsx
+++ src/Accessibility.tsx
@@ -10,7 +10,7 @@ function Accessibility() {
                         <p className="mb-4 leading-normal">Jobbsøknad Demo skal være tilgjengelig for alle. Det betyr at vi har som mål å følge lovpålagte krav til universell utforming. Vår ambisjon er i tillegg at du skal ha en god brukeropplevelse enten du bruker hjelpeteknologi (for eksempel skjermleser) eller ikke.</p>
 
                         <p className="mb-4 leading-normal">Alle virksomheter i offentlig sektor skal ha en tilgjengelighetserklæring. WCAG 2.1 på nivå AA er lovpålagt i Norge. Erklæringen beskriver hvert suksesskriterium i WCAG, og om nettstedet imøtekommer disse kravene.</p>
-                        <p className="mb-4 leading-normal">Ettersom Jobbsknad Demo er en demo, har vi valgt å ikke publisere en tilgjengelighetserklæring for denne per dags dato. Dette vil bli gjort dersom konseptet taes videre.</p>
+                        <p className="mb-4 leading-normal">Ettersom Jobbsknad Demo er en demo, har vi valgt å ikke publisere en tilgjengelighetserklæring for denne per dags dato. Dette vil bli gjort dersom konseptet videreføres.</p>
 
                         <h2 className="pb-4 pt-4 text-2xl">Feil, mangler og forbedringsforslag</h2>
                         <p className="leading-normal">Hvis du opplever problemer eller har forslag til forbedringer hører vi veldig gjerne fra deg! Feil og mangler kan rapporteres til <a className="underline" href="mailto:eilif.johansen@nav.no">eilif.johansen@nav.no</a>.</p>
