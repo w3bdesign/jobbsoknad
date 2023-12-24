b00d8dc4fbff7fc340eb384505dc84593ee59e47
EIlif Johansen <eilifjohansen@outlook.com>
2023-12-24 23:59:09 +0100

    Finjusterte personvernstekst

--- src/components/theme/Disclamer/About.tsx
+++ src/components/theme/Disclamer/About.tsx
@@ -5,7 +5,7 @@ function About() {
     return (
         <>
             <ul className="ListRemoveStyling pb-1">
-                <li><ShakeHands className="inline" title="Håndtrykk"/> Teksten lagres ikke, og sendes ikke til noen.</li>
+                <li><ShakeHands className="inline" title="Håndtrykk"/> Teksten blir ikke lagret, og sendes ikke til noen.</li>
                 <li><EyeScreened className="inline" title="Personvern"/> Ikke legg inn personopplysninger som navn, telefon og e-post i denne veilederen. Du kan legge til dette når du sender søknaden.</li>
             </ul>
         </>
